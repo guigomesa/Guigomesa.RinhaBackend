6a6b96d78092daeedfe7ecb39a01b289c8a17904
Guilherme Gomes <guigomesa@fastmail.com>
2023-08-12 21:35:45 +0000

    reset project

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Guigomesa.RinhaBackend.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
