21c25a5a072a2d454c61f8e16bf225694412d3ad
Guilherme Gomes <guigomesa@fastmail.com>
2023-08-12 22:19:22 +0000

    databse migration

---
+++ Migrations/20230812221739_inicial.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Guigomesa.RinhaBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pessoas",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Apelido = table.Column<string>(type: "character varying(32)", maxLength: 32, nullable: false),
+                    Nome = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Nascimento = table.Column<DateOnly>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pessoas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Stacks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    PessoaId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Nome = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stacks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Stacks_Pessoas_PessoaId",
+                        column: x => x.PessoaId,
+                        principalTable: "Pessoas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Stacks_PessoaId",
+                table: "Stacks",
+                column: "PessoaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Stacks");
+
+            migrationBuilder.DropTable(
+                name: "Pessoas");
+        }
+    }
+}
--- Models/Stack.cs
+++ Models/Stack.cs
@@ -1,9 +1,12 @@
 
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Models {
     public class Stack {
 
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public Guid Id {get;set;}
         public required Pessoa Pessoa {get;set;}
 
         [Required]
